2946a4558fb860c7e85b313aafeb5ee9719d9183
r-lassen <lassenrafaela@gmail.com>
2025-06-17 10:05:43 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+app.use(express.json()); //fazer a rota de post aceitar json
+
+app.post("/ia", async (req, res) => {
+  const { text } = req.body;
+  const response = await axios.post("http://localhost:11434/api/generate", {
+    model: "llama3",
+    prompt: text, //colocar pronpt 
+    stream: false,
+  });
+  const respData = response.data.response.toString()
+  res.send(respData);
+});
+
+
+//sigar servidor 
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});

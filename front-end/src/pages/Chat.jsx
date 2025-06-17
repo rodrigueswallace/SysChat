f97987798685efaf7e835a448587b19f6925de15
r-lassen <lassenrafaela@gmail.com>
2025-06-17 16:26:23 -0300

    feat: I completed the previous commission with React project settings

---
+++ front-end/src/pages/Chat.jsx
@@ -0,0 +1,24 @@
+const Chat = () => {
+  // parte da logica em javascript e chamada de api
+
+  return (
+    //codigo html e css
+    <>
+      <div className="min-h-screen bg-gradient-to-br bg-green-800 via-green-500 p-4 ">
+        <div className="max-auto max-w-x-4xl">
+          <header className="text-center text-green-10 mb-8">
+            <h1 className="test-xl font-bold">Chat Bay</h1>
+            <p className="text-white-600 text-lg pt-5">Bem-vindo ao Chat Bay, onde você pode interagir com o sistema dechat.
+            </p>
+          </header>
+
+          <div classeName="bg-white/70 backdrop-blur-sm rounded-2xl">
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Chat;

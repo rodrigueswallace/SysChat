64dc846a77e416118fffd229ee38fd3f5956760f
Wallace Rodrigues <wrodrigues@baymetrics.com.br>
2025-06-17 14:56:24 -0300

    feat: adding ddl and modifyng logical model and intance table

---
+++ database/physical_model/ddl.sql
@@ -0,0 +1,106 @@
+DROP SCHEMA IF EXISTS syschat CASCADE;
+CREATE SCHEMA syschat;
+
+
+CREATE TABLE syschat.user(
+    user_id INTEGER GENERATED ALWAYS AS IDENTITY,
+
+    user_photo_url VARCHAR(200),
+    user_name VARCHAR(10) NOT NULL,
+    user_lastName VARCHAR(20) NOT NULL,
+    user_email VARCHAR(20) NOT NULL,
+    user_password VARCHAR(128) NOT NULL,
+    user_email_canonical VARCHAR(100) NOT NULL,
+    user_start_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+
+    CONSTRAINT uq_s_syschat_t_user_c_user_email UNIQUE (user_email),
+    CONSTRAINT pk_s_syschat_t_user PRIMARY KEY (user_id),
+    CONSTRAINT ck_user_email_format CHECK (user_email LIKE '%@%.__%')
+);
+
+
+CREATE TABLE syschat.chat(
+    chat_id INTEGER GENERATED ALWAYS AS IDENTITY,
+
+    user_id INTEGER NOT NULL,
+    chat_name VARCHAR(20) NOT NULL,
+    chat_start_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+
+    CONSTRAINT pk_s_syschat_t_chat PRIMARY KEY (chat_id),
+    CONSTRAINT fk_chat_user
+     FOREIGN KEY (user_id)
+     REFERENCES syschat.user(user_id)
+     ON UPDATE RESTRICT
+     ON DELETE RESTRICT
+);
+
+
+CREATE TABLE syschat.msg(
+    msg_id INTEGER GENERATED ALWAYS AS IDENTITY,
+
+    chat_id INTEGER NOT NULL,
+    msg_author VARCHAR(20) NOT NULL,
+    msg_context TEXT,
+    msg_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+
+    CONSTRAINT pk_s_syschat_t_user_login PRIMARY KEY (msg_id),
+    CONSTRAINT fk_msg_chat
+     FOREIGN KEY (chat_id)
+     REFERENCES syschat.chat(chat_id)
+     ON UPDATE RESTRICT
+     ON DELETE RESTRICT
+);
+
+
+
+CREATE TABLE syschat.login_log(
+    ll_id INTEGER GENERATED ALWAYS AS IDENTITY,
+
+    user_id INTEGER NOT NULL,
+    ll_login_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_s_syschat_t_user_login_log PRIMARY KEY (ll_id),
+    CONSTRAINT fk_ll_user
+     FOREIGN KEY (user_id)
+     REFERENCES syschat.user(user_id)
+     ON UPDATE RESTRICT
+     ON DELETE RESTRICT
+
+
+);
+
+
+CREATE TABLE syschat.email_verification(
+    ev_id INTEGER GENERATED ALWAYS AS IDENTITY,
+
+    user_id INTEGER NOT NULL,
+    ev_code CHAR(6),
+    ev_create_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_s_syschat_t_user_login PRIMARY KEY (ev_id),
+    CONSTRAINT fk_ev_user
+     FOREIGN KEY (user_id)
+     REFERENCES syschat.user(user_id)
+     ON UPDATE RESTRICT
+     ON DELETE RESTRICT
+
+
+);
+
+
+CREATE INDEX idx_user_email ON syschat.user(user_email);
+CREATE INDEX idx_chat_user ON syschat.chat(user_id);
+CREATE INDEX idx_msg_chat ON syschat.msg(chat_id);
+CREATE INDEX idx_msg_date ON syschat.msg(msg_date);
+
+
+
+
+
+
+
+
+

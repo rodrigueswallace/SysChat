80094823aa273b8c7cc56802b24f5a161429f55f
Wallace Rodrigues <wrodrigues@baymetrics.com.br>
2025-06-25 10:10:16 -0300

    chore(): update in ddl correcting syntax

--- database/physical_model/ddl.sql
+++ database/physical_model/ddl.sql
@@ -2,7 +2,7 @@ DROP SCHEMA IF EXISTS syschat CASCADE;
 CREATE SCHEMA syschat;
 
 
-CREATE TABLE syschat.user(
+CREATE TABLE syschat.users(
     user_id INTEGER GENERATED ALWAYS AS IDENTITY,
 
     user_photo_url VARCHAR(200),
@@ -23,14 +23,14 @@ CREATE TABLE syschat.chat(
     chat_id INTEGER GENERATED ALWAYS AS IDENTITY,
 
     user_id INTEGER NOT NULL,
-    chat_name VARCHAR(20) NOT NULL,
+    chat_name VARCHAR(20) NOT NULL DEFAULT 'Chat',
     chat_start_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
 
     CONSTRAINT pk_s_syschat_t_chat PRIMARY KEY (chat_id),
     CONSTRAINT fk_chat_user
      FOREIGN KEY (user_id)
-     REFERENCES syschat.user(user_id)
+     REFERENCES syschat.users(user_id)
      ON UPDATE RESTRICT
      ON DELETE RESTRICT
 );
@@ -45,7 +45,7 @@ CREATE TABLE syschat.msg(
     msg_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
 
-    CONSTRAINT pk_s_syschat_t_user_login PRIMARY KEY (msg_id),
+    CONSTRAINT pk_s_syschat_t_msg PRIMARY KEY (msg_id),
     CONSTRAINT fk_msg_chat
      FOREIGN KEY (chat_id)
      REFERENCES syschat.chat(chat_id)
@@ -61,19 +61,18 @@ CREATE TABLE syschat.login_log(
     user_id INTEGER NOT NULL,
     ll_login_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT pk_s_syschat_t_user_login_log PRIMARY KEY (ll_id),
+    CONSTRAINT pk_s_syschat_t_login_log PRIMARY KEY (ll_id),
     CONSTRAINT fk_ll_user
      FOREIGN KEY (user_id)
-     REFERENCES syschat.user(user_id)
+     REFERENCES syschat.users(user_id)
      ON UPDATE RESTRICT
      ON DELETE RESTRICT
-
 
 );
 
 
-CREATE INDEX idx_user_email ON syschat.user(user_email);
-CREATE INDEX idx_chat_user ON syschat.chat(user_id);
+CREATE INDEX idx_user_email ON syschat.users(user_email);
+CREATE INDEX idx_chat_users ON syschat.chat(user_id);
 CREATE INDEX idx_msg_chat ON syschat.msg(chat_id);
 CREATE INDEX idx_msg_date ON syschat.msg(msg_date);
 
